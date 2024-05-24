712ed27f7fc11c0795c97bf23963241b589712ba
Aleksandra <aleksandra.stupiec@gmail.com>
2024-05-24 16:41:47 -0600

    CONNECTION WITH DB SUCCEDED!!!

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-  // Server response and console log messages
-  app.get('/', (req, res) => {
-    res.send('Hello, this is my Express server!');
-  });
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
- 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -28,3 +20,62 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 // app.use('/api', apiRoutes);
+
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'basedatospoliticalpower.database.windows.net',  
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'administrador', //update me
+                password: 'Te$91827364'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'PoliticalPowerDataBase'  //update me
+        }
+    }; 
+    
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement();  
+    });  
+    
+    connection.connect();
+
+
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+  
+    function executeStatement() {  
+        var request = new Request("EXEC getall_candidatos;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = "";  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                result+= column.value + " ";  
+              }  
+            });  
+            console.log(result);  
+            result ="";  
+        });  
+  
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        
+        // Close the connection after the final event emitted by the request, after the callback passes
+        request.on("requestCompleted", function (rowCount, more) {
+            connection.close();
+        });
+        connection.execSql(request);  
+    }
