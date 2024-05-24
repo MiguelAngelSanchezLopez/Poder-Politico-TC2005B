d36b6ce6da4fc11acf1e5bf9c1d248894e3a4a22
Aleksandra <aleksandra.stupiec@gmail.com>
2024-05-24 12:34:13 -0600

    node server running

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello, this is my Express server!');
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// app.use('/api', apiRoutes);
