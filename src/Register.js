e42e891438a8e6fcb2a697e907fc5f03eae0b92c
Aleksandra <aleksandra.stupiec@gmail.com>
2024-06-05 11:41:45 -0600

    changes in login and register path

--- src/Register.js
+++ src/Register.js
@@ -15,7 +15,7 @@ function Register() {
         setSuccess('');
 
         try {
-            const response = await fetch('http://localhost:3000/api/register', {
+            const response = await fetch('https://poder-politico.vercel.app/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
